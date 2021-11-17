d8c1bce8e0e91c42dde063383f95f298991d907c
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2021-11-17 17:25:58 -0300

    ajuste na geração manual de string

--- arraysStrings.c
+++ arraysStrings.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 void geraVetorString(char* vetor, int tam, int cont) {
@@ -11,14 +12,33 @@ void geraVetorString(char* vetor, int tam, int cont) {
     }
 }
 
-void vetorManualString(char* vetor, int tam, int cont) {	
+void copiaVetorString(char* vetorOriginal,char* vetorCopia,int tam, int cont){
 	if (cont < tam){
-		printf("%d -> ", cont+1);
-		scanf("%s", &vetor[cont]);		
-		vetorManualString(vetor, tam, ++cont);
+		vetorCopia[cont] = vetorOriginal[cont];
+		copiaVetorString(vetorOriginal, vetorCopia, tam, ++cont);
 	} else {
 		return;
 	}
+}
+
+void vetorManualString(char* vetor, int tam, int cont) {	
+	char aux[tam+ tam/2];
+	
+	int tamDigitado = 0;
+	
+	do {
+		if (tamDigitado > 0){
+			printf("\nDigitou %d letras, deve digitar uma palavra com %d letras: ", tamDigitado, tam);
+		} else {
+			printf("\nDigite sua palavra com %d letras: ", tam);	
+		}
+		scanf("%s", &aux);
+		
+		tamDigitado = strlen(aux);
+		
+	} while (tamDigitado != tam);
+	
+	copiaVetorString(aux, vetor, tam, 0);
 }
 
 void leArquivoString(char* vetor, int tam){
@@ -39,18 +59,9 @@ void leArquivoString(char* vetor, int tam){
     fclose(arquivo);
 }
 
-void copiaVetorString(char* vetorOriginal,char* vetorCopia,int tam, int cont){
-	if (cont < tam){
-		vetorCopia[cont] = vetorOriginal[cont];
-		copiaVetorString(vetorOriginal, vetorCopia, tam, ++cont);
-	} else {
-		return;
-	}
-}
-
 void printVetorString(char *vetor, int tam, int cont){
 	if (cont < tam-1) {
-		printf("%c, ", vetor[cont]);
+		printf("%c", vetor[cont]);
 	} else if (cont < tam) {
 		printf("%c", vetor[cont]);
 	} else {
